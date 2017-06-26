14a5fa48b019d4aa8a5542c97a318e38d7b04668
Farrukh <webstyle@protonmail.com>
2017-06-26 13:56:02 +0500

    added toolbar component and modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MdCardModule, MdListModule, MdToolbarModule} from '@angular/material';
+import 'hammerjs';
 
 import { AppComponent } from './app.component';
+import { ToolbarComponent } from './toolbar/toolbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ToolbarComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MdListModule,
+    MdCardModule,
+    MdToolbarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
